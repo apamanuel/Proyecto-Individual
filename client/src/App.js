4ed64c6400b98ed5abc9257641de5631fac70278
ManuelE <apamanuel@gmail.com>
2023-07-23 08:01:12 -0400

    Landing page without styles done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Landing from './components/Landing';
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <Routes>
+        <Route path ='/' element = {<Landing/>}/>
+        <Route path ='/home' element = {<h1>Soy el Home</h1>}/>
+        <Route path ='/detail' element = {<h1>Soy el Detail</h1>}/>
+        <Route path = '/form' element = {<h1>Soy el Formulario</h1>}/>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Landing = ()=>{
+    return (
+        <div className="Landing-page">
+            <h1>Bienvenidos a Videogames App</h1>
+            <button>Home</button>
+        </div>
+    );
+};
+
+export default Landing;
